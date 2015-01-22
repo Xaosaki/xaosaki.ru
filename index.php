ef1dd2337221dd78da2b570be851d9c95a82a7de
Xaosaki <xaosaki@gmail.com>
2015-01-22 23:42:23 +0300

    First

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php include('tpl/header.php')?>
+        <section>
+            <div class="block">
+                <h1>Основная информация</h1>
+                <div class="info">
+                    <p><strong>Меня зовут:</strong> Собакарь Александр Сергеевич</p>
+                    <p><strong>Мой возраст:</strong> 18 лет</p>
+                    <p><strong>Мой город:</strong> Севастополь, Россия</p>
+                    <p><strong>Моя специализация:</strong> Front/Back-end разработчик</p>
+                    <p><strong>Основные навыки:</strong> <span>html</span> <span>css</span>
+                        <span>php</span><span>mysql</span><span>javascript</span><span>PascalABC.NET</span></p>
+                </div>
+                <img class="avatar" src="style/img/avatar.png" width="149" height="149" alt="my_face.png"/>
+            </div>
+            <div class="block"><h1>Опыт работы</h1>
+                <div class="lline work"><p class="big">Фриланс Web Developer</p><p>Ноябрь 2012 - Апрель 2013</p></div>
+                <div class="lline work"><p class="big">Веб студия "Unio-services.com" — Web Developer</p><p>Июнь 2013 - Август 2014</p></div>
+                <div class="lline work"><p class="big">Проект "Vsemteam.ru" — Технический консультант</p><p>Июнь 2013 - по настоящее время</p></div>
+                <div class="lline work"><p class="big">Веб студия "Soul-team.ru" — Web Developer</p><p>Апрель 2013 - Июнь 2013. Декабрь 2014 - по настоящее время</p></div>
+            </div>
+            <div class="block"><h1>Образование</h1>
+                <div class="lline uni"><p class="big">Незаконченное высшее. Севастопольский Гос. Университет </p><p>Сентябрь 2014 - по настоящее время</p></div>
+                <div class="lline sert"><p class="big">Курсы Loftschool.ru</p><p>Январь 2015 - по настоящее время</p></div>
+            </div>
+        </section>
+<?php include('tpl/footer.php')?>
